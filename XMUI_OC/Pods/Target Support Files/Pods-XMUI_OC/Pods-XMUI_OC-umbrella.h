a6671458b1d8a8458c9fe88efeb1a76f5576c33f
ext.zhangmingwei1 <ext.zhangmingwei1@jd.com>
2022-05-26 12:48:32 +0800

    创建xcode项目添加空pod

---
+++ XMUI_OC/Pods/Target Support Files/Pods-XMUI_OC/Pods-XMUI_OC-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_XMUI_OCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XMUI_OCVersionString[];
+
---
+++ XMUI_OC/XMUI_OC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  XMUI_OC
+//
+//  Created by ext.zhangmingwei1 on 2022/5/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
