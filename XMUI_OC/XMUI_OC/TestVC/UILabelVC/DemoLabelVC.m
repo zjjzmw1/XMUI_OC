16ac463ab5fdb9ed9ade7bcc2d46bbba0a055ea3
ext.zhangmingwei1 <ext.zhangmingwei1@jd.com>
2022-05-27 18:48:35 +0800

    label frame实现最大宽度。

--- XMUI_OC/XMUI_OC/TestVC/UILabelVC/DemoLabelVC.m
+++ XMUI_OC/XMUI_OC/TestVC/UILabelVC/DemoLabelVC.m
@@ -19,6 +19,12 @@
 
 @property (nonatomic, strong) UILabel   *lbl3;
 
+@property (nonatomic, strong) UILabel   *lbl4;
+
+@property (nonatomic, strong) UILabel   *lbl5;
+
+@property (nonatomic, strong) UILabel   *lbl6;
+
 
 @end
 
@@ -58,6 +64,36 @@
     self.lbl3.frame = CGRectMake(20, self.lbl2.bottom + 20, kScreenWidth_XM - 40, 0);
     self.lbl3.numberOfLines = 0;
     [self.lbl3 setText:@"带行间距 6 的并且带上下左右边距的。。。。。。。多行上下左右边距的: 快捷创建label xm_contentInsets = UIEdgeInsetsMake(15, 10, 15, 10)快捷创建lab快捷创建lab快捷创建lab" lineSpacing:6];
+    
+    self.lbl4 = [UILabel getLabelWithFont:[UIFont systemFontOfSize:15] textColor:[UIColor redColor]];
+    [self.view addSubview:self.lbl4];
+    self.lbl4.xm_contentInsets = UIEdgeInsetsMake(15, 10, 15, 10);
+    self.lbl4.backgroundColor = [UIColor lightGrayColor];
+    self.lbl4.frame = CGRectMake(20, self.lbl3.bottom + 20, kScreenWidth_XM - 40, 80);
+    self.lbl4.text = @"长文不换行的不能设置sizeToFit 宽高都要设置。。。。frame的高度会把上下边距的覆盖了，左右边距还OK。";
+    
+    self.lbl5 = [UILabel getLabelWithFont:[UIFont systemFontOfSize:12] textColor:[UIColor redColor]];
+    [self.view addSubview:self.lbl5];
+    self.lbl5.xm_contentInsets = UIEdgeInsetsMake(5, 5, 5, 5);
+    self.lbl5.backgroundColor = [UIColor lightGrayColor];
+    self.lbl5.frame = CGRectMake(20, self.lbl4.bottom + 20, 200, 20);
+    self.lbl5.text = @"frame实现最大宽度的长文撒大哥放";
+    [self.lbl5 sizeToFit];
+    if (self.lbl5.frame.size.width > 200) {
+        self.lbl5.frame = CGRectMake(20, self.lbl4.bottom + 20, 200, 20);
+    }
+    
+    self.lbl6 = [UILabel getLabelWithFont:[UIFont systemFontOfSize:12] textColor:[UIColor redColor]];
+    [self.view addSubview:self.lbl6];
+    self.lbl6.xm_contentInsets = UIEdgeInsetsMake(5, 5, 5, 5);
+    self.lbl6.backgroundColor = [UIColor lightGrayColor];
+    self.lbl6.frame = CGRectMake(20, self.lbl5.bottom + 20, 200, 20);
+    self.lbl6.text = @"实现最大宽度";
+    [self.lbl6 sizeToFit];
+    if (self.lbl6.frame.size.width > 200) {
+        self.lbl6.frame = CGRectMake(20, self.lbl5.bottom + 20, 200, 20);
+    }
+
 }
 
 /*
